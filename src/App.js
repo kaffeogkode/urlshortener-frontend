524c9e6e3a7e615d976ba175f1dcf3d55394f4d1
Michael Banzon <michael@banzon.dk>
2020-11-15 22:06:44 +0100

    Links overview working 🚀🚢

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import Navbar from './Navbar'
+import './App.css'
+import 'bootstrap/dist/css/bootstrap.css'
+import { useEffect, useState } from 'react'
+import moment from 'moment'
 
-function App() {
+const App = () => {
+  const [ links, setLinks ] = useState([])
+  const [ showLinkVisits, setShowLinkVisits ] = useState(-1)
+
+  useEffect(() => {
+    fetch('http://localhost:9000/api/links')
+      .then(res => res.json())
+      .then(data => setLinks(data))
+      .catch(error => alert('Error getting links:', error))
+  }, [])
+
+  const handleLinkClick = id => {
+    setShowLinkVisits(id)
+  }
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <Navbar />
+
+      <p>{showLinkVisits}</p>
+
+      <div className="first-after-nav container">
+        <table className="table">
+          <thead className="thead-dark">
+            <tr>
+              <th>#</th>
+              <th>Hash / Short</th>
+              <th>Link</th>
+              <th>Created At</th>
+            </tr>
+          </thead>
+          <tbody>
+            {links.map(l => (
+              <tr key={l.id} onClick={_ => handleLinkClick(l.id)}>
+                <th>{l.id}</th>
+                <td>{l.hash}</td>
+                <td>{l.url}</td>
+                <td>
+                  <span title={moment.unix(l.created_at).toLocaleString('LLLL')}>
+                    {moment.unix(l.created_at).fromNow()}
+                  </span>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </>
+  )
 }
 
-export default App;
+export default App
